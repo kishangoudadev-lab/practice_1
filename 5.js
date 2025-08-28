d68c4fb451fc748230aa4dd51a2d618bb63183ad
Kishan <kishanhnr@gmail.com>
2025-08-28 22:14:06 +0530

    Modified

---
+++ 5.js
@@ -0,0 +1,37 @@
+function test(name,age,num){
+    return {
+        name : name,
+        age : age,
+        num : num
+
+    }
+}
+
+
+let x = test("kishan",25,12);
+console.log(x);
+
+
+
+let obj = {
+    name : "Kishan",
+    age : 25,
+    num : 12,
+    obj : {
+        req : "one",
+        ram : 16
+    },
+    arr : [1,2,3,99]
+};
+
+let y = JSON.parse(JSON.stringify(obj));
+
+let z = {...obj};
+
+obj.obj.req = "two";
+obj.arr[3] = 100;
+
+console.log(obj);
+console.log(y);
+console.log(z);
+
